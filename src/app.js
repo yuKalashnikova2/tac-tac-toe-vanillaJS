34614e10fdf4ab6922cf825f970135a223039202
YuliyaKalashnikova <pmalian@list.ru>
2023-02-22 13:55:53 +0700

    feat: generate grid

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+// import './style.css'
+
+const app = document.getElementById('app')
+
+const grid = document.createElement('div')
+grid.classList.add('grid')
+
+for (let i = 0; i < 9; i++) {
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+
+    cell.textContent = i
+    cell.dataset.id = i
+
+    grid.appendChild(cell)
+}
+
+app.appendChild(grid)
