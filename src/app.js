46bf152477b213dd9dd967ac5134eeace432808b
YuliyaKalashnikova <pmalian@list.ru>
2023-02-24 16:19:36 +0700

    refactor: create cell

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+let game = {
+    // xTurn: true,
+    i:0,
+    xState: [],
+    oState: [],
+    winnigState: [
+        [1, 5, 9],
+        [3, 5, 7],
+
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+    ]
+}
+
 const createGrid = () => {
     const grid = document.createElement('div')
     grid.classList.add('grid')
@@ -9,7 +28,40 @@ const createCell = (i) => {
     cell.classList.add('cell')
 
     cell.dataset.id = i
-    cell.addEventListener("click", () => console.log(i));
+    cell.addEventListener("click", (event) => {
+        const element = event.target
+        const id = element.dataset.id
+
+        if (!(id)) return
+        i++;
+        if (i % 2 === 0) {
+            game.oState.push(id)
+            element.classList.add('o')
+            element.classList.add('desabled')
+            
+        } else {
+            game.xState.push(id)
+            element.classList.add('x')
+            element.classList.add('desabled')
+        }
+        
+        // if (game.xTurn) {
+        //     game.xState.push(id)
+
+        //     element.classList.add('x')
+        //     element.classList.add('desabled')
+
+        //    game.xTurn = false
+        // } else {
+        //     game.oState.push(id)
+
+        //     element.classList.add('o')
+
+        //     game.xTurn = true
+        // }
+        // element.classList.add('disabled')
+        // console.log(id)
+    });
     return cell
 }
 
