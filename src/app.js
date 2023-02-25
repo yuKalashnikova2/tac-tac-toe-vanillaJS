069d54f6daa10561953cd78bcd22293b05da717d
YuliyaKalashnikova <pmalian@list.ru>
2023-02-25 10:55:38 +0700

    refactor: app.js

--- src/app.js
+++ src/app.js
@@ -1,82 +1,73 @@
-let game = {
-    // xTurn: true,
-    i:0,
-    xState: [],
-    oState: [],
-    winnigState: [
-        [1, 5, 9],
-        [3, 5, 7],
+const app = document.getElementById('app')
 
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
+const winnigState = [
+    [1, 5, 9],
+    [3, 5, 7],
 
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-    ]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+]
+
+let xState = [];
+let oState = [];
+
+let xTurn = true;
+
+const grid = document.createElement("div");
+grid.classList.add("grid");
+
+for (let i = 0; i < 9; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+
+    cell.dataset.id = i;
+
+    grid.appendChild(cell)
 }
 
-const createGrid = () => {
-    const grid = document.createElement('div')
-    grid.classList.add('grid')
-    return grid
+const result = document.createElement("div");
+result.classList.add("result");
+
+const resultText = document.createElement("p");
+resultText.textContent = 'Game over!';
+resultText.style.color = 'red'
+
+const restartButton = document.createElement("button");
+restartButton.textContent = 'Restart';
+restartButton.classList.add("btn")
+
+result.appendChild(resultText);
+result.appendChild(restartButton);
+
+app.appendChild(grid)
+
+// app.appendChild(result)
+
+const cellDiv = document.querySelectorAll('.cell');
+
+const drawCell = (element, xTurn) =>
+    xTurn ? element.classList.add('x') : element.classList.add('o');
+
+const saveTurn = (value, xTurn) =>
+    xTurn ? xState.push(value) : oState.push(value);
+
+const turn = (event) => {
+    const element = event.target;
+    if (element.classList.contains('x') || element.classList.contains('o'))
+        return;
+    
+    drawCell(element, xTurn);
+
+    saveTurn(parseInt(element.dataset.id), xTurn);
+
+    xTurn = !xTurn;
+
+    console.log(xState, oState)
 }
 
-const createCell = (i) => {
-    const cell = document.createElement('div')
-    cell.classList.add('cell')
-
-    cell.dataset.id = i
-    cell.addEventListener("click", (event) => {
-        const element = event.target
-        const id = element.dataset.id
-
-        if (!(id)) return
-        i++;
-        if (i % 2 === 0) {
-            game.oState.push(id)
-            element.classList.add('o')
-            element.classList.add('desabled')
-            
-        } else {
-            game.xState.push(id)
-            element.classList.add('x')
-            element.classList.add('desabled')
-        }
-        
-        // if (game.xTurn) {
-        //     game.xState.push(id)
-
-        //     element.classList.add('x')
-        //     element.classList.add('desabled')
-
-        //    game.xTurn = false
-        // } else {
-        //     game.oState.push(id)
-
-        //     element.classList.add('o')
-
-        //     game.xTurn = true
-        // }
-        // element.classList.add('disabled')
-        // console.log(id)
-    });
-    return cell
-}
-
-const generateGrid = () => {
-    const grid = createGrid()
-    for (let i = 0; i < 9; i++) {
-        const cell = createCell(i)
-        grid.appendChild(cell)
-    }
-    return grid
-}
-
-const initialApp = (app) => {
-    const grid = generateGrid()
-    app.appendChild(grid)
-}
-
-initialApp(document.getElementById('app'))
+cellDiv.forEach((cell) => cell.addEventListener('click', turn))
