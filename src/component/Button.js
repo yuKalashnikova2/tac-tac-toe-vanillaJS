35decf00bcc8747190f04de4b927c328a25223b9
YuliyaKalashnikova <pmalian@list.ru>
2023-03-01 15:51:29 +0700

    feat(component): Button

---
+++ src/component/Button.js
@@ -0,0 +1,11 @@
+export const Button = ({ text, onClick }) => {
+    const button = document.createElement('button');
+  
+    button.textContent = text;
+    if (typeof onClick === 'function') {
+      button.addEventListener('click', onClick);
+    }
+  
+    return button;
+  };
+  
