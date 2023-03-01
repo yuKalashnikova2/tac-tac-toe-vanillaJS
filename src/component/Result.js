938b21245fb8d265e2a293ea9ba3f046570b39d6
YuliyaKalashnikova <pmalian@list.ru>
2023-03-01 15:52:55 +0700

    feat(component): Result

---
+++ src/component/Result.js
@@ -0,0 +1,23 @@
+import { Button } from './Button';
+
+export const Result = ({ text, onClick }) => {
+  const result = document.createElement('div');
+  result.classList.add('result');
+
+  const resultText = document.createElement('p');
+  resultText.textContent = text;
+
+  const button = Button({
+    text: 'Restart game',
+    onClick: (event) => {
+      onClick(event);
+
+      result.remove();
+    },
+  });
+
+  result.appendChild(resultText);
+  result.appendChild(button);
+
+  return result;
+};
